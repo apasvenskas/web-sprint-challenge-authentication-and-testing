01c500fb337543ea8dd2e3510e429d3d60a9be92
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-11-07 16:38:38 -0800

    all code writen need to debug

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,18 @@
+
+const jwt = require('jsonwebtoken');
+
 module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+  if(!token){
+    return res.status(401).json({message: 'token required'})
+  }
+  jwt.verify(token, secret, (err, decoded) => {
+    if (err){
+      return res.status(401).json({message: "token invalid"})
+    }
+  })
   next();
+
   /*
     IMPLEMENT
 
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,46 @@
+const request = require('supertest');
+const server = require('./server.js');
+const users = require('./auth/auth-router.js');
+
+const validUser = {username: 'testuser', password: 'testpass'};
+const invalidUser = {username: 'wronguser', password: 'wrongpass'};
+const incompleteUser = {username: '', password: ''};
+
+
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+describe("Post register test", () => {
+  test('should register a new user and return 201 status', async () => {
+    const res = await request(server).post('/register').send(validUser);
+    expect(res.status).toBe(201)
+    expect(res.body).toHaveProperty('id')
+    expect(res.body).toHaveProperty('username')
+    expect(res.body).toHaveProperty('password')
+    })
+  test('should log in a valid user and return 200 status, welcome message and token',
+   async () => {
+    const res = await request(server).post('register').send(incompleteUser)
+    expect(res.status).toBe(400)
+    expect(res.body).toBe('username and password required')
+   })
+})
+
+describe('Post login test', () => {
+  it('should log in a valid user and return 200 status, welcome message and token', async () => {
+    await request(server).post('/register').send(validUser);
+    const res = await request(server).post('/login').send(validUser);
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty('message');
+    expect(res.body.message).toBe(`welcome, ${validUser.username}`);
+    expect(res.body).toHaveProperty('token');
+  });
+  it('should return 401 status and error message if invalid credentials are provided', async () => {
+    const res = await request(server).post('/login').send(invalidUser);
+    expect(res.status).toBe(401);
+    expect(res.body).toHaveProperty('message');
+    expect(res.body.message).toBe('invalid credentials');
+  });
 })
